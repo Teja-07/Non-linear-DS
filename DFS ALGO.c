9d7f598f149955e6662bd012aff13ff5569bf249
Teja chinitha <82213085+Teja-07@users.noreply.github.com>
2021-07-04 10:30:47 +0530

    Add files via upload

---
+++ DFS ALGO.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int vertex;
+  struct node* next;
+};
+
+struct node* createNode(int v);
+
+struct Graph {
+  int numVertices;
+  int* visited;
+
+  // We need int** to store a two dimensional array.
+  // Similary, we need struct node** to store an array of Linked lists
+  struct node** adjLists;
+};
+
+// DFS algo
+void DFS(struct Graph* graph, int vertex) {
+  struct node* adjList = graph->adjLists[vertex];
+  struct node* temp = adjList;
+
+  graph->visited[vertex] = 1;
+  printf("Visited %d \n", vertex);
+}
